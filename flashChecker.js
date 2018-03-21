fba043a3b7875e43e3263895e539e71b50b268e5
Ryu <katosun2@gmail.com>
2018-03-21 16:48:50 +0800

    优化IE获取Flash版本号

--- flashChecker.js
+++ flashChecker.js
@@ -1,66 +1,70 @@
 /*!
  * 
  * 检测和获取浏览器flash版本
- * @Version 1.0.0
+ * @Version 1.0.1
  * @return {hasFlash: hasFlash, v: flashVersion}
  *
  */
 
 (function(window, undefined) {
-	"use strict";
+    "use strict";
 
-	var flashChecker = function() {
-		/*是否安装了flash*/
-		var hasFlash = 0,
-			/*flash版本*/
-			flashVersion = 0,
-			/*是否IE浏览器*/
-			isIE = (!!window.ActiveXObject) || (!!navigator.userAgent.match(/Trident.*rv\:11\./));
+    var flashChecker = function() {
+        /*是否安装了flash*/
+        var hasFlash = 0,
+            /*flash版本*/
+            flashVersion = 0,
+            /*是否IE浏览器*/
+            isIE = (!!window.ActiveXObject) || (!!navigator.userAgent.match(/Trident.*rv\:11\./));
 
-		if (isIE) {
-			try{
-				var swf = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-				if (swf) {
-					hasFlash = 1;
-					flashVersion = swf.GetVariable("$version");
-				}
-			}catch(e){}
-		} else {
-			if (navigator.plugins && navigator.plugins.length > 0) {
-				var swf = navigator.plugins['Shockwave Flash'];
+        if (isIE) {
+            try{
+                var swf = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+                if (swf) {
+                    hasFlash = 1;
+                    flashVersion = swf.GetVariable("$version");
 
-				if (swf) {
-					var desc = swf.description.split(' ');
+                    /* 转换为数字 */
+                    flashVersion = flashVersion.toString().split(',')[0];
+                    flashVersion = Number(flashVersion.replace(/[^\d]+/, ''));
+                }
+            }catch(e){}
+        } else {
+            if (navigator.plugins && navigator.plugins.length > 0) {
+                var swf = navigator.plugins['Shockwave Flash'];
 
-					hasFlash = 1;
+                if (swf) {
+                    var desc = swf.description.split(' ');
 
-					/*获取版本号，一般情况*/
-					for(var i = 0; i < desc.length; i++){
-						if(Number(desc[i]) > 0){
-							flashVersion = Number(desc[i]);
-							break;
-						}
-					}
-				}
-			}
-		}
+                    hasFlash = 1;
 
-		return {
-			hasFlash: !!hasFlash,
-			ver: flashVersion
-		};
-	}
+                    /*获取版本号，一般情况*/
+                    for(var i = 0; i < desc.length; i++){
+                        if(Number(desc[i]) > 0){
+                            flashVersion = Number(desc[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-	if (typeof define === 'function' && define.amd) {
-		define(function() {
-			return flashChecker;
-		});
-	} else if (typeof module === 'object' && module && typeof module.exports === 'object' && module.exports) {
-		module.exports = flashChecker;
-	} else {
-		window.flashChecker = flashChecker;
-	}
+        return {
+            hasFlash: !!hasFlash,
+            ver: flashVersion
+        };
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return flashChecker;
+        });
+    } else if (typeof module === 'object' && module && typeof module.exports === 'object' && module.exports) {
+        module.exports = flashChecker;
+    } else {
+        window.flashChecker = flashChecker;
+    }
 
 })(function() {
-	return this || window;
+    return this || window;
 }());
